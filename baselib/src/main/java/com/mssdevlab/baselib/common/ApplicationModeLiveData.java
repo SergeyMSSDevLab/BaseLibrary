bc6b3b37297ce30e06767ff743d4ad3d3c8c64e4
Sergei Matiunin <sergei.matiunin@kraftvaerk.com>
2018-08-24 15:39:13 +0300

    Promote dialog refactored

--- baselib/src/main/java/com/mssdevlab/baselib/common/ApplicationModeLiveData.java
+++ baselib/src/main/java/com/mssdevlab/baselib/common/ApplicationModeLiveData.java
@@ -1,14 +1,23 @@
 package com.mssdevlab.baselib.common;
 
 import android.arch.lifecycle.LiveData;
+import android.os.Looper;
 
 public class ApplicationModeLiveData extends LiveData<AppMode> {
 
     private static final ApplicationModeLiveData sAppMode = new ApplicationModeLiveData();
 
     private ApplicationModeLiveData() {
-        this.setValue(AppMode.MODE_DEMO);
+        this.setValueInternal(AppMode.MODE_DEMO);
         // TODO: complete
+    }
+
+    private void setValueInternal(AppMode val){
+        if (Looper.myLooper() == Looper.getMainLooper()){
+            this.setValue(val);
+        } else {
+            this.postValue(val);
+        }
     }
 
     public static LiveData<AppMode> applicationMode(){
