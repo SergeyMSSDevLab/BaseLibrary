6c5a8cacfdcea1b90259024c674e1fb3325d14c9
Sergei Matiunin <sergei.matiunin@kraftvaerk.com>
2018-11-08 15:29:33 +0300

    Refactored

---
+++ baselib/src/main/java/com/mssdevlab/baselib/ads/InterestialManager.java
@@ -0,0 +1,9 @@
+package com.mssdevlab.baselib.ads;
+
+import androidx.annotation.StringRes;
+
+public class InterestialManager {
+    public static void enableAds(@StringRes int unitId){
+
+    }
+}
